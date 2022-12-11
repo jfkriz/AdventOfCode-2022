05ab1176720ea4d29ad1371c9f702e66c09f200e
Jim Kriz <jim@kriz.net>
2022-12-10 20:53:58 -0500

    More enhancements to Point

--- src/test/kotlin/util/Point.kt
+++ src/test/kotlin/util/Point.kt
@@ -3,12 +3,8 @@ package util
 data class Point<T>(var x: Int, var y: Int, var value: T) {
     fun move(direction: Direction): Point<T> =
         this.apply {
-            when (direction) {
-                Direction.Up -> y--
-                Direction.Down -> y++
-                Direction.Left -> x--
-                Direction.Right -> x++
-            }
+            x += direction.xOffset
+            y += direction.yOffset
         }
 
     fun move(xCount: Int, yCount: Int): Point<T> =
@@ -19,25 +15,29 @@ data class Point<T>(var x: Int, var y: Int, var value: T) {
 
     fun isSameLocation(other: Point<*>) = this.x == other.x && this.y == other.y
 
-    fun isNeighboringLocation(other: Point<*>) = listOf(
-        listOf(x, y - 1), // Up
-        listOf(x, y + 1), // Down
-        listOf(x - 1, y), // Left
-        listOf(x + 1, y), // Right
-        listOf(x - 1, y - 1), // Up+Left
-        listOf(x - 1, y + 1), // Down+Left
-        listOf(x + 1, y - 1), // Up+Right
-        listOf(x + 1, y + 1), // Down+Right
-    ).any {
-        it[0] == other.x && it[1] == other.y
-    }
+    fun isNeighboringLocation(other: Point<*>, includeDiagonal: Boolean = true) =
+        Direction.values().filter {
+            if (includeDiagonal) {
+                true
+            } else {
+                !it.diagonal
+            }
+        }.map {
+            (x + it.xOffset) to (y + it.yOffset)
+        }.any {
+            it.first == other.x && it.second == other.y
+        }
 
     fun differenceWith(other: Point<*>) = (this.x - other.x) to (this.y - other.y)
 }
 
-enum class Direction(val xOffset: Int, val yOffset: Int) {
+enum class Direction(val xOffset: Int, val yOffset: Int, val diagonal: Boolean = false) {
     Up(0, -1),
     Down(0, 1),
     Left(-1, 0),
-    Right(1, 0)
+    Right(1, 0),
+    UpLeft(-1, -1, true),
+    DownLeft(-1, 1, true),
+    UpRight(1, -1, true),
+    DownRight(1, 1, true)
 }
