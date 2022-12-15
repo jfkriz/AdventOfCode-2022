5b7e33d69c7aa215d2586d2f8a74cd045780c787
Jim Kriz <jim@kriz.net>
2022-12-14 20:18:12 -0500

    Day 14 - Regolith Reservoir
    I'm not very happy with this solution, but didn't have much time to do better.

---
+++ src/test/kotlin/day14/RegolithReservoirTest.kt
@@ -0,0 +1,187 @@
+package day14
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import util.DataFiles
+import util.Direction
+import util.collections.Matrix
+
+@Suppress("SpellCheckingInspection")
+@DisplayName("Day 14 - Regolith Reservoir")
+@TestMethodOrder(OrderAnnotation::class)
+class RegolithReservoirTest : DataFiles() {
+    private val sampleSolver by lazy {
+        Solver(loadSampleInput())
+    }
+    private val solver by lazy {
+        Solver(loadInput())
+    }
+
+    @Test
+    @Order(1)
+    fun `Part 1 Sample Input should return 24`() {
+        assertEquals(24, sampleSolver.solvePartOne())
+    }
+
+    @Test
+    @Order(3)
+    fun `Part 2 Sample Input should return 93`() {
+        assertEquals(93, sampleSolver.solvePartTwo())
+    }
+
+    @Test
+    @Order(2)
+    fun `Part 1 Real Input should return 638`() {
+        assertEquals(638, solver.solvePartOne())
+    }
+
+    @Test
+    @Order(4)
+    fun `Part 2 Real Input should return 31722`() {
+        assertEquals(31722, solver.solvePartTwo())
+    }
+}
+
+class Solver(data: List<String>) {
+    private val reservoir = Reservoir(data)
+    private val reservoirPartTwo = Reservoir(data, true)
+    fun solvePartOne(): Int {
+        return reservoir.dropSandGrains(0 to 500)
+    }
+
+    fun solvePartTwo(): Int {
+        return reservoirPartTwo.dropSandGrains(0 to 500)
+    }
+}
+
+class Reservoir(input: List<String>, private val infiniteFloor: Boolean = false) {
+    private var grid: Matrix<Tile>
+    private var minX: Int
+    private var maxX: Int
+    private var minY: Int
+    private var maxY: Int
+
+    init {
+        val instructions = input.map { line ->
+            line.split(Regex(" -> ")).map { point ->
+                val (y, x) = point.split(",").map(Integer::parseInt)
+                x to y
+            }
+        }
+
+        minX = instructions.flatten().minOf { it.first }
+        maxX = instructions.flatten().maxOf { it.first }
+        minY = instructions.flatten().minOf { it.second }
+        maxY = instructions.flatten().maxOf { it.second }
+        val width = maxY + 2
+        val height = maxX + 2
+
+        grid = Matrix(width, height, Tile.Open)
+
+        if (infiniteFloor) {
+            // HACK ALERT!!! I could not get the auto-expansion to work, so I just used an arbitrarily
+            // large number for expansion on the right of the grid to hopefully get it working. It
+            // did, but ugh... I may come back to this sometime after the holidays.
+            grid = grid.expand(right = 1000, fill = Tile.Open).expand(down = 1, fill = Tile.Rock)
+            maxX = grid.height
+            minY = 0
+            maxY = grid.width
+        }
+
+        instructions.forEach { rock ->
+            createRockFormation(rock)
+        }
+    }
+
+    fun dropSandGrains(firstGrain: Pair<Int, Int>): Int {
+        var dropped = 0
+        var currentGrain: Pair<Int, Int>?
+        var dropFrom = firstGrain
+        do {
+            try {
+                currentGrain = dropSandGrain(dropFrom)
+                if (currentGrain != null) {
+                    grid.setPoint(currentGrain.first, currentGrain.second, Tile.Sand)
+                    dropped++
+                } else {
+                    if (infiniteFloor) {
+                        grid = grid.expand(left = 10, right = 10, fill = Tile.Open)
+                        grid.drawLine(grid.height - 1 to 0, grid.height - 1 to grid.width - 1, Tile.Rock)
+                        dropFrom = dropFrom.first to dropFrom.second + 10
+                        maxY = grid.width
+                        currentGrain = dropFrom
+                    }
+                }
+            } catch (e: HoleIsPluggedException) {
+                dropped++
+                currentGrain = null
+            }
+        } while (currentGrain != null)
+
+        println()
+        println(grid.toString())
+        return dropped
+    }
+
+    private fun createRockFormation(rock: List<Pair<Int, Int>>) {
+        rock.windowed(2, 1).forEach {
+            val (start, end) = it
+            grid.drawLine(start, end, Tile.Rock)
+        }
+    }
+
+    private fun dropSandGrain(currentXY: Pair<Int, Int>): Pair<Int, Int>? {
+        var found = true
+        var nextXY: Pair<Int, Int> = currentXY
+
+        while (found) {
+            val possibleTiles =
+                grid.getNeighboringPoints(nextXY.first, nextXY.second, includeDiagonal = true) { _, neighbor ->
+                    neighbor.value == Tile.Open &&
+                        neighbor.x <= maxX &&
+                        neighbor.y >= minY &&
+                        neighbor.y <= maxY
+                }.filter {
+                    it.key == Direction.Down || it.key == Direction.DownLeft || it.key == Direction.DownRight ||
+                        it.key == Direction.Left || it.key == Direction.Right
+                }
+
+            if (possibleTiles.filterNot { it.key == Direction.Left || it.key == Direction.Right }.isNotEmpty()) {
+                for (dir in listOf(Direction.Down, Direction.DownLeft, Direction.DownRight)) {
+                    if (possibleTiles.containsKey(dir)) {
+                        found = true
+                        nextXY = possibleTiles[dir]!!.x to possibleTiles[dir]!!.y
+                        break
+                    }
+                }
+            } else {
+                found = false
+            }
+        }
+
+        return if (nextXY.first == currentXY.first && nextXY.second == currentXY.second) {
+            if (infiniteFloor) {
+                throw HoleIsPluggedException(currentXY)
+            }
+            null
+        } else if (nextXY.first >= maxX || nextXY.second <= minY || nextXY.second >= maxY) {
+            null
+        } else {
+            nextXY
+        }
+    }
+}
+
+class HoleIsPluggedException(startingPoint: Pair<Int, Int>) : Exception("Hole is plugged up: $startingPoint")
+
+enum class Tile(private val symbol: Char) {
+    Rock('#'),
+    Sand('o'),
+    Open('.');
+
+    override fun toString() = symbol.toString()
+}
